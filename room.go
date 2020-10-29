48e65a21a0a4627ac9c9974c22873a9afe61f69b
ham357 <sakuraniumarete@gmail.com>
2020-10-29 23:12:02 +0900

    チャットルームのモデル化

--- room.go
+++ room.go
@@ -3,4 +3,10 @@ package main
 type room struct {
 	//forwardは他のクライアントに転送するためのメッセージを保持するチャネルです
 	forward chan []byte
+	//joinはチャットルームに参加しようとしているクライアントのためのチャネルです。
+	join chan *client
+	//leaveはチャットルームから退室しようとしているクライアントのためのチャネルです。
+	leave chan *client
+	//clientsには材質しているすべてのクライアントが保持されます。
+	clients map[*client]bool
 }
