7bd673b02b83d68a672763cadbb96dcb36b37b72
ham357 <sakuraniumarete@gmail.com>
2020-11-01 00:15:31 +0900

    Webサーバーのポート番号を指定できるように修正

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+	"flag"
 )
 
 type templateHandler struct {
@@ -18,17 +19,20 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	//チャットルームを開始します
 	go r.run()
 	//Webサーバーを開始します
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバーを開始します。ポート:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
