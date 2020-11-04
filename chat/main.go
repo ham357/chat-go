f3f59d2ba012a0b968b2a7f4c093fd815c41c8ec
ham357 <sakuraniumarete@gmail.com>
2020-11-05 00:18:42 +0900

    ユーザ名の表示と環境変数の設定

--- chat/main.go
+++ chat/main.go
@@ -10,6 +10,11 @@ import (
 	"os"
 
 	"github.com/ham357/chat-go/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -18,16 +23,32 @@ type templateHandler struct {
 	templ    *template.Template
 }
 
+// ServeHTTPはHTTPリクエストを処理します
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+	// Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("hQGvCPja7f")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/github"),
+		google.New(os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_SERCRET"), "http://localhost:8080/auth/callback/google"),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/ham357/chat-go
+
+go 1.15
+
+require (
+	github.com/clbanning/x2j v0.0.0-20191024224557-825249438eec // indirect
+	github.com/gorilla/websocket v1.4.2
+	github.com/stretchr/codecs v0.0.0-20170403063245-04a5b1e1910d // indirect
+	github.com/stretchr/gomniauth v0.0.0-20170717123514-4b6c822be2eb
+	github.com/stretchr/goweb v0.0.0-20140611034857-d7518e7a1cf8 // indirect
+	github.com/stretchr/objx v0.3.0
+	github.com/stretchr/signature v0.0.0-20160104132143-168b2a1e1b56 // indirect
+	github.com/stretchr/stew v0.0.0-20130812190256-80ef0842b48b // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/stretchr/tracer v0.0.0-20140124184152-66d3696bba97 // indirect
+	github.com/ugorji/go v1.1.13 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
+)
