33c6c017d095085d5ec4c78a983389f8f5cd0334
ham357 <sakuraniumarete@gmail.com>
2020-11-05 09:56:51 +0900

    チャット画面に送信時間を表示

--- chat/client.go
+++ chat/client.go
@@ -22,7 +22,7 @@ func (c *client) read() {
 	for {
 		var msg *message
 		if err := c.socket.ReadJSON(&msg); err == nil {
-			msg.When = time.Now()
+			msg.When = time.Now().Format("2006/1/2 15:04:05")
 			msg.Name = c.userData["name"].(string)
 			c.room.forward <- msg
 		} else {
--- chat/message.go
+++ chat/message.go
@@ -1,12 +1,8 @@
 package main
 
-import (
-	"time"
-)
-
 // messageは1つのメッセージを表します。
 type message struct {
 	Name    string
 	Message string
-	When    time.Time
+	When    string
 }
